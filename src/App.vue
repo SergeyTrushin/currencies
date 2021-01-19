4e903495fb1fe6e48b0826b2437b006fabfd9969
SergeyTrushin <sergeytrushin-17@yandex.ru>
2021-01-19 21:02:57 +0400

    currencies

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <div class="main">
+      <v-simple-table fixed-header>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <td>Название валюты</td>
+            <td>Цена </td>
+            <td>Количество</td>
+          </tr>
+        </thead>
+        <tbody v-if="show">
+          <tr v-for="currency in currencies" :key="currency.name">
+            <td>{{currency.name}}</td>
+            <td>{{Math.round(currency.volume)}}</td>
+            <td>{{currency.price.amount.toFixed(2)}}</td>
+          </tr>
+        </tbody>
+        <tbody v-else> 
+          <tr>
+            <td class="empty" colspan="3" loading>
+              Нет данных
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </div>
+</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+    },
+    data: () => ({
+     currencies: [],
+     show: false
+   }),
+    mounted() {
+      fetch('http://phisix-api3.appspot.com/stocks.json')
+      .then(resp => resp.json())
+      .then(data => { this.currencies = data.stock; this.show = true })
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .main{
+    border: 1px solid #ccc;
+    padding: 20px;
+    width: 70%;
+    margin: 30px auto;
+    border-radius: 5px;
+  }
+  thead{
+    color: rgba(0,0,0,.6);
+  }
+  thead td{
+    font-size: 12px!important;
+  }
+  td{
+    height: 50px!important;
+  }
+  .empty{
+    text-align: center;
+  }
 </style>
